260a3b34cea891b2cad289f22bf0c1fa77a055bc
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2020-12-18 10:10:50 -0500

    added the navbar to the files as well as starting on the news page

--- genNews.py
+++ genNews.py
@@ -7,7 +7,19 @@ def getCurrentDate():
     formattedDate = today.strftime('%d/%m/%Y')
     return formattedDate
 
+def updatePastNews():
+    with open('./web/assets/News.json', 'rt') as readNewsFile:
+        news = json.loads(readNewsFile.read())
 
+    with open('./web/assets/PastNews.json', 'rt') as readPastNewsFile:
+        pastNews = json.loads(readPastNewsFile.read())
+
+    newsKey = news.get('Title')
+    pastNews.update({newsKey: {'News': news.get('News'), 'Date': news.get('Date')}})
+
+    with open('./web/assets/PastNews.json', 'wt') as writePastNewsFile:
+        writePastNewsFile.write(json.dumps(pastNews, indent=2))
 
 if __name__ == "__main__":
+    updatePastNews()
     print(getCurrentDate())
