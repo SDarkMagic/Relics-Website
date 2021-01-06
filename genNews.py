0aecfe172f3c4386234e4d2fb8e17ea7ab6305ca
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2021-01-06 16:30:09 -0500

    Added FTP support to auto update the News on the website

--- genNews.py
+++ genNews.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import pathlib
+import ftplib
 import datetime
 
 def getCurrentDate():
@@ -37,7 +38,23 @@ def writeCurrentNews(title, author):
     with open('./web/assets/News.json', 'wt') as writeNewsJson:
         writeNewsJson.write(json.dumps(newsData, indent=2))
 
+def uploadNewsFiles(fileName, fileObj):
+    with open('privars.json', 'rt') as readCredentials:
+        credentials = json.loads(readCredentials.read())['ftpDetails']
+    ftp = ftplib.FTP(credentials.get('host'))
+    ftp.login(user=credentials.get('user'), passwd=credentials.get('password'))
+    ftp.storbinary(f'STOR {fileName}', open(fileObj, 'rb'))
+    print(f'Successfully transferred {fileName} to the server!')
+    try:
+        ftp.quit()
+    except:
+        ftp.close()
+
 if __name__ == "__main__":
+    currentNewsFile = pathlib.Path('./web/assets/News.json')
+    pastNewsFile = pathlib.Path('./web/assets/PastNews.json')
     updatePastNews()
     writeCurrentNews(sys.argv[1], sys.argv[2])
     print(getCurrentDate())
+    uploadNewsFiles(currentNewsFile.name, currentNewsFile)
+    uploadNewsFiles(pastNewsFile.name, pastNewsFile)
